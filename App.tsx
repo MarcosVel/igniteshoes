2cca6ab7218802b82f8fd007486f81803aea012c
MarcosVel <marcosvelosodev@gmail.com>
2023-10-18 00:41:44 -0300

    refactor: navigate throw notification

--- App.tsx
+++ App.tsx
@@ -6,10 +6,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import { NativeBaseProvider } from "native-base";
 import { StatusBar } from "react-native";
-import OneSignal, {
-  NotificationReceivedEvent,
-  OSNotification,
-} from "react-native-onesignal";
+import OneSignal from "react-native-onesignal";
 
 import { Routes } from "./src/routes";
 
@@ -17,26 +14,11 @@ import { Loading } from "./src/components/Loading";
 import { THEME } from "./src/theme";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
-import { useEffect, useState } from "react";
-import { Notification } from "./src/components/Notification";
 
 OneSignal.setAppId(ONESIGNAL_APP_ID);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-  const [notification, setNotification] = useState<OSNotification>();
-
-  useEffect(() => {
-    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler(
-      (notificationReceivedEvent: NotificationReceivedEvent) => {
-        const response = notificationReceivedEvent.getNotification();
-
-        setNotification(response);
-      }
-    );
-
-    return () => unsubscribe;
-  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -48,13 +30,6 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
-
-      {notification?.title && (
-        <Notification
-          title={notification.title}
-          onClose={() => setNotification(undefined)}
-        />
-      )}
     </NativeBaseProvider>
   );
 }
